0ce0fed944906064be33d3dbb6fc0e3b8110596b
Aleks Gusev <algusb@mail.ru>
2022-10-17 11:13:06 +0300

    edit test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,8 +1,12 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -11,17 +15,22 @@ class CardDeliveryTest {
     String generateDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
+
     @Test
     void shouldFillOutForm() {
         String date = generateDate(3);
-        open("http://0.0.0.0:7777/");
+        open("http://localhost:7777/");
         ElementsCollection items = $$(".tab-item");
         $("[data-test-id=city] input").setValue("Иваново");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(date);
         $("[data-test-id=name] input").setValue("Иванов  Иван");
         $("[data-test-id=phone] input").setValue("+79998887766");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id=notification]").shouldHave(matchText("Успешно!"), Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + date), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
